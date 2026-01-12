f0f0f17b70f44f75c1bdc601f70333139c4f2ca7
sanjanapadashetti <sanjanapadashetti@gmail.com>
2026-01-13 04:55:11 +0530

    Fixed requirements filename

---
+++ Car.py
@@ -0,0 +1,19 @@
+class Car:
+    def __init__(self, brand, model, year, price):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.price = price
+
+    def show_details(self):
+        print("Car Details")
+        print("-----------")
+        print("Brand :", self.brand)
+        print("Model :", self.model)
+        print("Year  :", self.year)
+        print("Price : Rs.", self.price)
+
+
+if __name__ == "__main__":
+    car = Car("Toyota", "Innova", 2022, 2500000)   # object name should be different
+    car.show_details()
